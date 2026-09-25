c131d75a085096605de7c6133af5cbcde2685aae
MarcoCodispoti <marco01codispoti@gmail.com>
2026-09-25 14:49:57 +0200

    Correzione dello stato enumerato errato "PING" in quello corretto "NORMAL"

--- core/states.py
+++ core/states.py
@@ -5,8 +5,8 @@ from enum import Enum
 # Definisco la classe che andrà a enumerare i tipi di messaggi che i nodi si scambieranno
 class NodeState(Enum):
     # Tipi di messaggi defini come stringhe costanti
+    NORMALE = "NORMAL"
     ANSWER = "ANSWER"
     COORDINATOR = "COORDINATOR"
     ELECTION = "ELECTION"
-    PING = "PING"
 
